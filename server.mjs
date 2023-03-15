e2627f4a2006691942c9c2390b604e7d8f243a19
axiesearch <axiesearch@gmail.com>
2023-03-15 10:30:29 -0700

    update

--- server.mjs
+++ server.mjs
@@ -1,38 +1,12 @@
 import { createServer } from 'http';
+import {Hero} from '@ulixee/hero';
 
 createServer((req, res) => {
-  const Hero = require('@ulixee/hero');
-
-  const fs = require('fs');
-  const path = require("path");
 
   (async () => {
     const hero = new Hero()
     await hero.goto('https://www.southwest.com/air/booking/select.html?adultPassengersCount=1&adultsCount=1&departureDate=2023-05-25&departureTimeOfDay=NOON_TO_SIX&destinationAirportCode=SNA&fareType=USD&int=HOMEQBOMAIR&originationAirportCode=SJC&passengerType=ADULT&reset=true&returnDate=2023-05-28&returnTimeOfDay=NOON_TO_SIX&tripType=roundtrip');
     await sleep(20000);
-
-    const directory = 'screenshots';
-    const screenshot = await hero.takeScreenshot({
-      format: 'jpeg',
-      jpegQuality: 100,
-      fullPage: true,
-    });
-
-    const timestamp = new Date().toISOString().replace(/:/g, '-');
-    const filename = `image-${timestamp}.jpg`;
-
-    // Check if the directory exists, and create it if it doesn't
-    if (!fs.existsSync(directory)) {
-      fs.mkdirSync(directory);
-    }
-
-    fs.writeFile(path.join(directory, filename), screenshot, function(err) {
-      if (err) {
-        console.log('Error saving image:', err);
-      } else {
-        console.log(`Image saved as ${filename} in ${directory} directory successfully`);
-      }
-    });
 
     await hero.close();
   })();
